28f13e3bb8ba88462197d09501fc65701020b5a8
GaoLiHai <1019933576@qq.com>
2021-02-01 19:05:27 +0800

    feature: 核心层实现了推出群聊的功能

---
+++ src/core/quitGroup.js
@@ -0,0 +1,32 @@
+const errCode = require('../util/errCode');
+const axios = require('axios').default;
+const { URL } = require('url');
+const errorHandler = require('../util/errorHandler');
+
+/**
+ * @description 退出群聊
+ * @param {string} baseUrl    mirai-api-http server 的地址
+ * @param {string} sessionKey 会话标识
+ * @param {number} target     欲推出的群号
+ * @returns {Object} 结构 { message, code }
+ */
+module.exports = async ({ baseUrl, sessionKey, target }) => {
+    try {
+        // 拼接 url
+        const url = new URL('/quit', baseUrl).toString();
+
+        // 请求
+        let {
+            data: { msg: message, code }
+        } = await axios.post(url, { sessionKey, target });
+
+        // 抛出 mirai 的异常，到 catch 中处理后再抛出
+        if (code in errCode) {
+            throw new Error(message);
+        }
+        return { message, code };
+    } catch (error) {
+        errorHandler(error);
+    }
+
+}
