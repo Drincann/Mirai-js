5d1f3b2bfc0a87e672b12a418b33a3de668fbd05
GaoLiHai <1019933576@qq.com>
2021-02-01 15:20:43 +0800

    style: 修改注释格式

--- src/core/getGroupList.js
+++ src/core/getGroupList.js
@@ -5,8 +5,8 @@ const errorHandler = require('../util/errorHandler');
 
 /**
  * @description 获取群列表
- * @param {string}             baseUrl      mirai-api-http server 的地址
- * @param {string}             sessionKey   会话标识
+ * @param {string} baseUrl    mirai-api-http server 的地址
+ * @param {string} sessionKey 会话标识
  * @returns {array[Object]} 结构 array[...{ id, name, permission }]
  */
 module.exports = async ({ baseUrl, sessionKey }) => {
