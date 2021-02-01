299dc866f0d40d4c1317c9ec8266e10183391dcc
GaoLiHai <1019933576@qq.com>
2021-02-01 19:26:06 +0800

    feature: 核心层实现了解除全员禁言的功能

---
+++ src/core/unmuteAll.js
@@ -0,0 +1,31 @@
+const errCode = require('../util/errCode');
+const axios = require('axios').default;
+const { URL } = require('url');
+const errorHandler = require('../util/errorHandler');
+
+/**
+ * @description 解除全员禁言
+ * @param {string} baseUrl    mirai-api-http server 的主机地址
+ * @param {string} sessionKey 会话标识
+ * @param {number} target     欲解除全员禁言的群号
+ * @returns {Object} 结构 { message, code }
+ */
+module.exports = async ({ baseUrl, sessionKey, target }) => {
+    try {
+        // 拼接 URL
+        const url = new URL('/unmuteAll', baseUrl).toString();
+
+        // 请求
+        let {
+            data: { code, msg: message }
+        } = await axios.post(url, { sessionKey, target });
+
+        // 抛出 mirai 的异常，到 catch 中处理后再抛出
+        if (code in errCode) {
+            throw new Error(message);
+        }
+        return { message, code };
+    } catch (error) {
+        errorHandler(error);
+    }
+}
