d1cf3d1964e8a70de146942519b54498e6962a47
GaoLiHai <1019933576@qq.com>
2021-02-14 15:55:39 +0800

    feature: 核心层实现了获取群配置 to #8

---
+++ src/core/getGroupConfig.js
@@ -0,0 +1,30 @@
+const errCode = require('../util/errCode');
+const axios = require('axios').default;
+const { URL } = require('url');
+const errorHandler = require('../util/errorHandler');
+
+/**
+ * @description 获取群配置
+ * @param {string} baseUrl    mirai-api-http server 的地址
+ * @param {string} sessionKey 会话标识
+ * @param {string} target     群号
+ * @returns {Object} 
+ */
+module.exports = async ({ baseUrl, sessionKey, target }) => {
+    try {
+        // 拼接 url
+        const url = new URL('/groupConfig', baseUrl).toString();
+
+        // 请求
+        const { data } = await axios.get(url, { params: { sessionKey, target } });
+        const { msg: message, code } = data;
+
+        // 抛出 mirai 的异常，到 catch 中处理后再抛出
+        if (code in errCode) {
+            throw new Error(message);
+        }
+        return data;
+    } catch (error) {
+        errorHandler(error);
+    }
+}
