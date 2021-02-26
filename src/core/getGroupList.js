dc4d01d320ec1fe603b03c330bed655b87c9175e
queuecat <2356583659@qq.com>
2021-02-26 23:20:10 +0800

    getGroupList错误处理

--- src/core/getGroupList.js
+++ src/core/getGroupList.js
@@ -15,7 +15,12 @@ module.exports = async ({ baseUrl, sessionKey }) => {
         const url = new URL('/groupList', baseUrl).toString();
 
         // 请求
-        const { data } = await axios.get(url, { params: { sessionKey } });
+        const responseData = await axios.get(url, { params: { sessionKey } });
+        try {
+            var { data } = responseData;
+        } catch (error) {
+            throw new Error('core.getGroupList 请求返回格式出错，请检查 mirai-console')
+        }
         const { msg: message, code } = data;
 
         // 抛出 mirai 的异常，到 catch 中处理后再抛出
