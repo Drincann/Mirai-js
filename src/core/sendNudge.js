2d79bd034b84f7176993935480b58c5f47cb2953
GaoLiHai <1019933576@qq.com>
2021-03-29 13:26:27 +0800

    feature: 核心层实现了戳一戳 - sendNudge

---
+++ src/core/sendNudge.js
@@ -0,0 +1,39 @@
+const { errCodeMap } = require('../util/errCode');
+const axios = require('axios').default;
+const { URL } = require('url');
+const errorHandler = require('../util/errorHandler');
+
+/**
+ * @description 发送戳一戳消息
+ * @param {string} baseUrl    mirai-api-http server 的地址
+ * @param {string} sessionKey 会话标识
+ * @param {number} target     戳一戳的目标
+ * @param {number} subject    戳一戳的上下文，群或好友
+ * @param {string} kind       上下文类型, 可选值 Friend, Group
+ * @returns {Object} 结构 { message, code, messageId }
+ */
+module.exports = async ({ baseUrl, sessionKey, target, subject, kind }) => {
+    try {
+        // 拼接 url
+        const url = new URL('/sendNudge', baseUrl).toString();
+
+        // 请求
+        const responseData = await axios.post(url, {
+            sessionKey, target, subject, kind
+        });
+        try {
+            var {
+                data: { msg: message, code }
+            } = responseData;
+        } catch (error) {
+            throw new Error('core.sendNudge 请求返回格式出错，请检查 mirai-console');
+        }
+        // 抛出 mirai 的异常，到 catch 中处理后再抛出
+        if (code in errCodeMap) {
+            throw new Error(message);
+        }
+        return { message, code };
+    } catch (error) {
+        errorHandler(error);
+    }
+};
