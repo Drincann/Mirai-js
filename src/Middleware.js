0067106b5264f9412f9c3d349f30dba229f5fa20
GaoLiHai <1019933576@qq.com>
2021-04-08 16:36:54 +0800

    feature: 同步中间件机制 close #26

--- src/Middleware.js
+++ src/Middleware.js
@@ -550,7 +550,7 @@ class Middleware {
             return new Promise(resolve => {
                 // 从右侧递归合并中间件链
                 this.middleware.reduceRight((next, middleware) => {
-                    return () => middleware(data, next);
+                    return async () => await middleware(data, next);
                 }, async () => {
                     // 最深层递归，即开发者提供的回调函数
                     let returnVal = callback instanceof Function ? (await callback(data)) : undefined;
