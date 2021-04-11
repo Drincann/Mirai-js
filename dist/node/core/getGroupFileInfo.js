4ae00c474cf10bf85eaeaaf731b39269f575d872
GaoLiHai <1019933576@qq.com>
2021-04-11 14:47:16 +0800

    feature: core 文件操作 getGroupFileInfo.js to #31

--- dist/node/core/getGroupFileInfo.js
+++ dist/node/core/getGroupFileInfo.js
@@ -18,12 +18,16 @@ if (!process.browser) {
 
 const errorHandler = require('../util/errorHandler');
 /**
- * @description 获取群列表
+ * @description 获取群文件详细信息
  * @param {string} baseUrl    mirai-api-http server 的地址
  * @param {string} sessionKey 会话标识
- * @param {string} group      群号
- * @param {string} dir        可选，查询目录，默认为根目录
- * @returns {Object[]}   结构 array[...{ id, name, path, isFile }]
+ * @param {number} target     群号
+ * @param {string} id         文件 id
+ * @returns {Object}   
+ * 结构 { 
+ *     id, name, path, length, downloadTimes, uploaderId, 
+ *     uploadTime, lastModifyTime, downloadUrl, sha1, md5
+ * }
  */
 
 
@@ -31,17 +35,17 @@ module.exports = async ({
   baseUrl,
   sessionKey,
   target,
-  dir
+  id
 }) => {
   try {
     // 拼接 url
-    const url = new URL('/groupFileList', baseUrl).toString(); // 请求
+    const url = new URL('/groupFileInfo', baseUrl).toString(); // 请求
 
     const responseData = await axios.get(url, {
       params: {
         sessionKey,
         target,
-        dir
+        id
       }
     });
 
@@ -54,7 +58,7 @@ module.exports = async ({
         }
       } = responseData;
     } catch (error) {
-      throw new Error('core.getGroupFileList 请求返回格式出错，请检查 mirai-console');
+      throw new Error('core.getGroupFileInfo 请求返回格式出错，请检查 mirai-console');
     } // 抛出 mirai 的异常，到 catch 中处理后再抛出
 
 
