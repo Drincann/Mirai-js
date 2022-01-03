bb17b5805465cab9f037d570fb3841be4bf9653f
Drincann <1019933576@qq.com>
2022-01-03 14:49:33 +0800

    feature: 实现了 Bot.removeFriend 删除好友

--- src/Bot.js
+++ src/Bot.js
@@ -22,6 +22,7 @@ const _muteAll = require('./core/muteAll');
 const _unmute = require('./core/unmute');
 const _unmuteAll = require('./core/unmuteAll');
 const _removeMember = require('./core/removeMember');
+const _removeFriend = require('./core/removeFriend');
 const _quitGroup = require('./core/quitGroup');
 const _getGroupConfig = require('./core/getGroupConfig');
 const _setGroupConfig = require('./core/setGroupConfig');
@@ -859,12 +860,33 @@ class Bot extends BotConfigGetable {
     }
 
     /**
+     * @description 删除好友
+     * @param {*} qq 欲删除的好友 qq 号
+     */
+    async removeFriend({ qq }) {
+        // 检查对象状态
+        if (!this.config) {
+            throw new Error('removeFriend 请先调用 open，建立一个会话');
+        }
+
+        // 检查参数
+        if (!qq) {
+            throw new Error('removeFriend 缺少必要的 qq 参数');
+        }
+
+        const { baseUrl, sessionKey } = this.config;
+
+        // 删除好友
+        await _removeFriend({ baseUrl, sessionKey, target: qq });
+    }
+
+    /**
      * @description 移除群成员
      * @param {number} group   必选，欲移除的成员所在群号
      * @returns {void}
      */
     async quitGroup({ group }) {
-        // 检查对象状态
+        // 检查对象状态 
         if (!this.config) {
             throw new Error('quitGroup 请先调用 open，建立一个会话');
         }
---
+++ src/core/removeFriend.js
@@ -0,0 +1,44 @@
+const { errCodeMap } = require('../util/errCode');
+const axios = require('axios').default;
+let URL;
+if (!process.browser) {
+    ({ URL } = require('url'));
+} else {
+    URL = window.URL;
+}
+const errorHandler = require('../util/errorHandler');
+const path = require('path');
+const locationStr = `core.${path.basename(__filename, path.extname(__filename))}`;
+
+/**
+ * @description 删除好友
+ * @param {string} baseUrl    mirai-api-http server 的地址
+ * @param {string} sessionKey 会话标识
+ * @param {number} target   欲删除好友的 qq 号
+ * @returns {Object} 结构 { message, code }
+ */
+module.exports = async ({ baseUrl, sessionKey, target }) => {
+    try {
+        // 拼接 url
+        const url = new URL('/deleteFriend', baseUrl).toString();
+
+        // 请求
+        const responseData = await axios.post(url, { sessionKey, target });
+        try {
+            var {
+                data: { msg: message, code }
+            } = responseData;
+        } catch (error) {
+            throw new Error(('请求返回格式出错，请检查 mirai-console'));
+        }
+        // 抛出 mirai 的异常，到 catch 中处理后再抛出
+        if (code in errCodeMap) {
+            throw new Error(message);
+        }
+        return { message, code };
+    } catch (error) {
+        console.error(`mirai-js: error ${locationStr}`);
+        errorHandler(error);
+    }
+
+};
