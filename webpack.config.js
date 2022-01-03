d659bd0c5eba518d68b521af0760f02c8e391765
Drincann <1019933576@qq.com>
2022-01-03 14:48:07 +0800

    chore(webpack): 修复 ws v8+ 带来的打包问题

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         filename: 'mirai-js.js',
         clean: true
     },
+    externals: {
+        // ws v8+ 会需要配置更多 polyfill, 这里通过外部依赖直接忽略它
+        ws: 'ws'
+    },
     plugins: [
         new ProvidePlugin({
             process: 'process/browser',
