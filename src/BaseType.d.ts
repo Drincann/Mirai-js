4163ebf550db73a5b0c72133fb0943743a34e4b9
Mantou1233 <rwkn100@gmail.com>
2022-08-02 18:41:31 +0800

    better typechecking

--- src/BaseType.d.ts
+++ src/BaseType.d.ts
@@ -1,63 +1,66 @@
+import { Bot } from "./Bot";
+
+type MessageChainTypes = "Source" | "Quote" | "At" | "AtAll" | "Face" | "Plain" | "Image" | "FlashImage" | "Voice" | "Xml" | "Json" | "App" | "Poke" | "Dice" | "MarketFace" | "MusicShare" | "ForwardMessage" | "File" | "MiraiCode"
 /**
  * 消息链的元素，是 mirai-api-http 接口需要的原始类型
  * 有多种消息类型实现了该接口，已经全部列出
- * 
+ *
  * @see https://github.com/project-mirai/mirai-api-http/blob/master/docs/MessageType.md
  */
 interface MessageType {
-    type: string;
+	type: MessageChainTypes;
 
-    // Quote
-    id?: number,
-    groupId?: number,
-    senderId?: number,
-    targetId?: number,
-    origin?: MessageType[];
+	// Quote
+	id?: number;
+	groupId?: number;
+	senderId?: number;
+	targetId?: number;
+	origin?: MessageType[];
 
-    // At
-    target?: number;
-    display?: string;
+	// At
+	target?: number;
+	display?: string;
 
-    // Face Poke
-    faceId?: number;
-    name?: string;
+	// Face Poke
+	faceId?: number;
+	name?: string;
 
-    // Plain
-    text?: string;
+	// Plain
+	text?: string;
 
-    // Image FlashImage Voice
-    imageId?: ImageId;
-    voiceId?: VoiceId;
-    url?: string;
-    path?: string;
+	// Image FlashImage Voice
+	imageId?: ImageId;
+	voiceId?: VoiceId;
+	url?: string;
+	path?: string;
 
-    // Xml
-    xml?: string;
+	// Xml
+	xml?: string;
 
-    // Json
-    json?: string;
+	// Json
+	json?: string;
 
-    // App
-    content?: string;
+	// App
+	content?: string;
 
-    // Forward
-    nodeList?: ForwardNode[];
+	// Forward
+	nodeList?: ForwardNode[];
 }
 
 // 用于 Bot 获得消息链，Message 使用了该扩展
 interface MessageChainGetable {
-    getMessageChain(): MessageType[];
+	getMessageChain(): MessageType[];
 }
 
 /**
  * @description 合并转发类型Forward内消息节点的类型定义
  */
 interface ForwardNode {
-    senderId?: number;
-    time?: number;
-    senderName?: string;
-    messageChain?: MessageType[] | MessageChainGetable;
-    messageId?: MessageId;
+	senderId?: number;
+	time?: number;
+	senderName?: string;
+	messageChain?: MessageType[] | MessageChainGetable;
+	messageId?: MessageId;
 }
 
 /**
@@ -65,10 +68,10 @@ interface ForwardNode {
  * 的途径，避免其他类直接访问实现，用来解耦
  */
 interface BotConfigGetable {
-    getBaseUrl(): string;
-    getQQ(): number;
-    getVerifyKey(): string;
-    getSessionKey(): string;
+	getBaseUrl(): string;
+	getQQ(): number;
+	getVerifyKey(): string;
+	getSessionKey(): string;
 }
 
 
@@ -81,116 +84,376 @@ type MessageId = number;
 
 /**
  * 消息类型
- * 
+ *
  * @see https://github.com/project-mirai/mirai-api-http/blob/master/docs/EventType.md
  */
-type EventType =
-    // WebSocket 事件
-    | 'error' | 'close' | 'unexpected-response'
-    // mirai 事件
-    | 'GroupMessage' | 'FriendMessage'
-    | 'BotOnlineEvent' | 'BotOfflineEventActive'
-    | 'BotOfflineEventForce' | 'BotOfflineEventDropped'
-    | 'BotReloginEvent' | 'BotGroupPermissionChangeEvent'
-    | 'BotMuteEvent' | 'BotUnmuteEvent'
-    | 'BotJoinGroupEvent' | 'BotLeaveEventActive'
-    | 'BotLeaveEventKick' | 'GroupRecallEvent'
-    | 'FriendRecallEvent' | 'GroupNameChangeEvent'
-    | 'GroupEntranceAnnouncementChangeEvent' | 'GroupMuteAllEvent'
-    | 'GroupAllowAnonymousChatEvent' | 'GroupAllowConfessTalkEvent'
-    | 'GroupAllowMemberInviteEvent' | 'MemberJoinEvent'
-    | 'MemberLeaveEventKick' | 'MemberLeaveEventQuit'
-    | 'MemberCardChangeEvent' | 'MemberSpecialTitleChangeEvent'
-    | 'MemberPermissionChangeEvent' | 'MemberMuteEvent'
-    | 'MemberUnmuteEvent' | 'NewFriendRequestEvent'
-    | 'MemberJoinRequestEvent' | 'BotInvitedJoinGroupRequestEvent';
+
+interface BaseType {
+	bot: Bot;
+}
+
+// Middleware
+interface MessageExtendType{
+    text?: string,
+    classified?: {
+        [key in MessageChainTypes]?: any[]
+    }
+    messageId?: number,
+    unlock?: () => void
+}
+
+type RequestEventMethods = "agree" | "refuse" | "ignore" | "refuseAndAddBlacklist" | "ignoreAndAddBlacklist"
+
+type RequestEventExtendType = {
+    [key in RequestEventMethods]?: () => void
+}
+
+interface EventTypes {
+	error: {
+		code: number;
+	} & BaseType;
+	close: {
+		reason: string;
+	} & BaseType;
+	"unexpected-response": {
+		reason: string;
+	} & BaseType;
+	GroupMessage: {
+		type: "GroupMessage";
+		sender: {
+			id: number;
+			memberName: string;
+			specialTitle: string;
+			permission: keyof typeof Bot.groupPermission;
+			joinTimestamp: number;
+			lastSpeakTimestamp: number;
+			muteTimeRemaining: number;
+			group: {
+				id: number;
+				name: string;
+				permission: keyof typeof Bot.groupPermission;
+			};
+		};
+		messageChain: MessageType[];
+	} & BaseType &
+		MessageExtendType;
+	FriendMessage: BaseType;
+	BotOnlineEvent: BaseType;
+	BotOfflineEventActive: BaseType;
+	BotOfflineEventForce: BaseType;
+	BotOfflineEventDropped: BaseType;
+	BotReloginEvent: BaseType;
+	BotGroupPermissionChangeEvent: BaseType;
+	BotMuteEvent: BaseType;
+	BotUnmuteEvent: BaseType;
+	BotJoinGroupEvent: BaseType;
+	BotLeaveEventActive: BaseType;
+	BotLeaveEventKick: BaseType;
+	GroupRecallEvent: BaseType;
+	FriendRecallEvent: BaseType;
+	GroupNameChangeEvent: BaseType;
+	GroupEntranceAnnouncementChangeEvent: BaseType;
+	GroupMuteAllEvent: BaseType;
+	GroupAllowAnonymousChatEvent: BaseType;
+	GroupAllowConfessTalkEvent: BaseType;
+	GroupAllowMemberInviteEvent: BaseType;
+	MemberJoinEvent: BaseType;
+	MemberLeaveEventKick: BaseType;
+	MemberLeaveEventQuit: BaseType;
+	MemberCardChangeEvent: BaseType;
+	MemberSpecialTitleChangeEvent: BaseType;
+	MemberPermissionChangeEvent: BaseType;
+	MemberMuteEvent: BaseType;
+	MemberUnmuteEvent: BaseType;
+	NewFriendRequestEvent: BaseType;
+	MemberJoinRequestEvent: BaseType;
+	BotInvitedJoinGroupRequestEvent: BaseType;
+}
+
+type EventType = keyof EventTypes
+
+type Awaitable<T> = T | PromiseLike<T>
 
 // 群成员权限
-type GroupPermission =
-    | 'OWNER'
-    | 'ADMINISTRATOR'
-    | 'MEMBER';
+type GroupPermission = "OWNER" | "ADMINISTRATOR" | "MEMBER";
 
 // 性别
-type SEX = 'UNKNOWN' | 'MALE' | 'FEMALE'
-
-// 消息处理器
-type Processor = (data: any) => Promise<any> | any;
+type SEX = "UNKNOWN" | "MALE" | "FEMALE";
 
 // QQ 自带表情
 type FaceType =
-    | '惊讶' | '撇嘴' | '色' | '发呆' | '得意'
-    | '流泪' | '害羞' | '闭嘴' | '睡' | '大哭'
-    | '尴尬' | '发怒' | '调皮' | '呲牙' | '微笑'
-    | '难过' | '酷' | '抓狂' | '吐' | '偷笑'
-    | '可爱' | '白眼' | '傲慢' | '饥饿' | '困'
-    | '惊恐' | '流汗' | '憨笑' | '悠闲' | '奋斗'
-    | '咒骂' | '疑问' | '嘘' | '晕' | '折磨'
-    | '衰' | '骷髅' | '敲打' | '再见' | '发抖'
-    | '爱情' | '跳跳' | '猪头' | '拥抱' | '蛋糕'
-    | '闪电' | '炸弹' | '刀' | '足球' | '便便'
-    | '咖啡' | '饭' | '玫瑰' | '凋谢' | '爱心'
-    | '心碎' | '礼物' | '太阳' | '月亮' | '赞'
-    | '踩' | '握手' | '胜利' | '飞吻' | '怄火'
-    | '西瓜' | '冷汗' | '擦汗' | '抠鼻' | '鼓掌'
-    | '糗大了' | '坏笑' | '左哼哼' | '右哼哼' | '哈欠'
-    | '鄙视' | '委屈' | '快哭了' | '阴险' | '左亲亲'
-    | '吓' | '可怜' | '菜刀' | '啤酒' | '篮球'
-    | '乒乓' | '示爱' | '瓢虫' | '抱拳' | '勾引'
-    | '拳头' | '差劲' | '爱你' | '不' | '好'
-    | '转圈' | '磕头' | '回头' | '跳绳' | '挥手'
-    | '激动' | '街舞' | '献吻' | '左太极' | '右太极'
-    | '双喜' | '鞭炮' | '灯笼' | 'K歌' | '喝彩'
-    | '祈祷' | '爆筋' | '棒棒糖' | '喝奶' | '飞机'
-    | '钞票' | '药' | '手枪' | '茶' | '眨眼睛'
-    | '泪奔' | '无奈' | '卖萌' | '小纠结' | '喷血'
-    | '斜眼笑' | 'doge' | '惊喜' | '骚扰' | '笑哭'
-    | '我最美' | '河蟹' | '羊驼' | '幽灵' | '蛋'
-    | '菊花' | '红包' | '大笑' | '不开心' | '冷漠'
-    | '呃' | '好棒' | '拜托' | '点赞' | '无聊'
-    | '托脸' | '吃' | '送花' | '害怕' | '花痴'
-    | '小样儿' | '飙泪' | '我不看' | '托腮' | '啵啵'
-    | '糊脸' | '拍头' | '扯一扯' | '舔一舔' | '蹭一蹭'
-    | '拽炸天' | '顶呱呱' | '抱抱' | '暴击' | '开枪'
-    | '撩一撩' | '拍桌' | '拍手' | '恭喜' | '干杯'
-    | '嘲讽' | '哼' | '佛系' | '掐一掐' | '惊呆'
-    | '颤抖' | '啃头' | '偷看' | '扇脸' | '原谅'
-    | '喷脸' | '生日快乐' | '头撞击' | '甩头' | '扔狗'
-    | '加油必胜' | '加油抱抱' | '口罩护体' | '搬砖中' | '忙到飞起'
-    | '脑阔疼' | '沧桑' | '捂脸' | '辣眼睛' | '哦哟'
-    | '头秃' | '问号脸' | '暗中观察' | 'emm' | '吃瓜'
-    | '呵呵哒' | '我酸了' | '太南了' | '辣椒酱' | '汪汪'
-    | '汗' | '打脸' | '击掌' | '无眼笑' | '敬礼'
-    | '狂笑' | '面无表情' | '摸鱼' | '魔鬼笑' | '哦'
-    | '请' | '睁眼' | '敲开心' | '震惊' | '让我康康'
-    | '摸锦鲤' | '期待' | '拿到红包' | '真好' | '拜谢'
-    | '元宝' | '牛啊' | '胖三斤' | '好闪' | '左拜年'
-    | '右拜年' | '红包包' | '右亲亲' | '牛气冲天' | '喵喵'
-    | '求红包' | '谢红包' | '新年烟花' | '打call' | '变形'
-    | '嗑到了' | '仔细分析' | '加油' | '我没事' | '菜狗'
-    | '崇拜' | '比心' | '庆祝' | '老色痞' | '拒绝'
-    | '嫌弃' | '吃糖';
+	| "惊讶"
+	| "撇嘴"
+	| "色"
+	| "发呆"
+	| "得意"
+	| "流泪"
+	| "害羞"
+	| "闭嘴"
+	| "睡"
+	| "大哭"
+	| "尴尬"
+	| "发怒"
+	| "调皮"
+	| "呲牙"
+	| "微笑"
+	| "难过"
+	| "酷"
+	| "抓狂"
+	| "吐"
+	| "偷笑"
+	| "可爱"
+	| "白眼"
+	| "傲慢"
+	| "饥饿"
+	| "困"
+	| "惊恐"
+	| "流汗"
+	| "憨笑"
+	| "悠闲"
+	| "奋斗"
+	| "咒骂"
+	| "疑问"
+	| "嘘"
+	| "晕"
+	| "折磨"
+	| "衰"
+	| "骷髅"
+	| "敲打"
+	| "再见"
+	| "发抖"
+	| "爱情"
+	| "跳跳"
+	| "猪头"
+	| "拥抱"
+	| "蛋糕"
+	| "闪电"
+	| "炸弹"
+	| "刀"
+	| "足球"
+	| "便便"
+	| "咖啡"
+	| "饭"
+	| "玫瑰"
+	| "凋谢"
+	| "爱心"
+	| "心碎"
+	| "礼物"
+	| "太阳"
+	| "月亮"
+	| "赞"
+	| "踩"
+	| "握手"
+	| "胜利"
+	| "飞吻"
+	| "怄火"
+	| "西瓜"
+	| "冷汗"
+	| "擦汗"
+	| "抠鼻"
+	| "鼓掌"
+	| "糗大了"
+	| "坏笑"
+	| "左哼哼"
+	| "右哼哼"
+	| "哈欠"
+	| "鄙视"
+	| "委屈"
+	| "快哭了"
+	| "阴险"
+	| "左亲亲"
+	| "吓"
+	| "可怜"
+	| "菜刀"
+	| "啤酒"
+	| "篮球"
+	| "乒乓"
+	| "示爱"
+	| "瓢虫"
+	| "抱拳"
+	| "勾引"
+	| "拳头"
+	| "差劲"
+	| "爱你"
+	| "不"
+	| "好"
+	| "转圈"
+	| "磕头"
+	| "回头"
+	| "跳绳"
+	| "挥手"
+	| "激动"
+	| "街舞"
+	| "献吻"
+	| "左太极"
+	| "右太极"
+	| "双喜"
+	| "鞭炮"
+	| "灯笼"
+	| "K歌"
+	| "喝彩"
+	| "祈祷"
+	| "爆筋"
+	| "棒棒糖"
+	| "喝奶"
+	| "飞机"
+	| "钞票"
+	| "药"
+	| "手枪"
+	| "茶"
+	| "眨眼睛"
+	| "泪奔"
+	| "无奈"
+	| "卖萌"
+	| "小纠结"
+	| "喷血"
+	| "斜眼笑"
+	| "doge"
+	| "惊喜"
+	| "骚扰"
+	| "笑哭"
+	| "我最美"
+	| "河蟹"
+	| "羊驼"
+	| "幽灵"
+	| "蛋"
+	| "菊花"
+	| "红包"
+	| "大笑"
+	| "不开心"
+	| "冷漠"
+	| "呃"
+	| "好棒"
+	| "拜托"
+	| "点赞"
+	| "无聊"
+	| "托脸"
+	| "吃"
+	| "送花"
+	| "害怕"
+	| "花痴"
+	| "小样儿"
+	| "飙泪"
+	| "我不看"
+	| "托腮"
+	| "啵啵"
+	| "糊脸"
+	| "拍头"
+	| "扯一扯"
+	| "舔一舔"
+	| "蹭一蹭"
+	| "拽炸天"
+	| "顶呱呱"
+	| "抱抱"
+	| "暴击"
+	| "开枪"
+	| "撩一撩"
+	| "拍桌"
+	| "拍手"
+	| "恭喜"
+	| "干杯"
+	| "嘲讽"
+	| "哼"
+	| "佛系"
+	| "掐一掐"
+	| "惊呆"
+	| "颤抖"
+	| "啃头"
+	| "偷看"
+	| "扇脸"
+	| "原谅"
+	| "喷脸"
+	| "生日快乐"
+	| "头撞击"
+	| "甩头"
+	| "扔狗"
+	| "加油必胜"
+	| "加油抱抱"
+	| "口罩护体"
+	| "搬砖中"
+	| "忙到飞起"
+	| "脑阔疼"
+	| "沧桑"
+	| "捂脸"
+	| "辣眼睛"
+	| "哦哟"
+	| "头秃"
+	| "问号脸"
+	| "暗中观察"
+	| "emm"
+	| "吃瓜"
+	| "呵呵哒"
+	| "我酸了"
+	| "太南了"
+	| "辣椒酱"
+	| "汪汪"
+	| "汗"
+	| "打脸"
+	| "击掌"
+	| "无眼笑"
+	| "敬礼"
+	| "狂笑"
+	| "面无表情"
+	| "摸鱼"
+	| "魔鬼笑"
+	| "哦"
+	| "请"
+	| "睁眼"
+	| "敲开心"
+	| "震惊"
+	| "让我康康"
+	| "摸锦鲤"
+	| "期待"
+	| "拿到红包"
+	| "真好"
+	| "拜谢"
+	| "元宝"
+	| "牛啊"
+	| "胖三斤"
+	| "好闪"
+	| "左拜年"
+	| "右拜年"
+	| "红包包"
+	| "右亲亲"
+	| "牛气冲天"
+	| "喵喵"
+	| "求红包"
+	| "谢红包"
+	| "新年烟花"
+	| "打call"
+	| "变形"
+	| "嗑到了"
+	| "仔细分析"
+	| "加油"
+	| "我没事"
+	| "菜狗"
+	| "崇拜"
+	| "比心"
+	| "庆祝"
+	| "老色痞"
+	| "拒绝"
+	| "嫌弃"
+	| "吃糖";
 
 // 消息处理器类型
 export {
-    // 接口
-    MessageChainGetable, BotConfigGetable,
-
-    // 消息类型
-    MessageType,
-
-    // MessageType.nodeList 的元素类型
-    ForwardNode,
-
-    // 图片 id  语音 id  消息 id
-    ImageId, VoiceId, MessageId,
-
-    // 事件类型    群成员权限      性别
-    EventType, GroupPermission, SEX,
-
-    // 消息处理器
-    Processor,
-
-    // QQ 自带表情
-    FaceType
+	// 接口
+	MessageChainGetable,
+	BotConfigGetable,
+	// 消息类型
+	MessageType,
+	// MessageType.nodeList 的元素类型
+	ForwardNode,
+	// 图片 id  语音 id  消息 id
+	ImageId,
+	VoiceId,
+	MessageId,
+	// 事件类型    群成员权限      性别
+	EventType,
+    EventTypes,
+	GroupPermission,
+	SEX,
+	// QQ 自带表情
+	FaceType,
+    Awaitable
 };
