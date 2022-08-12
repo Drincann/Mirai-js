3c18b6b4e884cc9a45a1957c7dce631f148f5719
Drincann <1019933576@qq.com>
2022-08-12 16:50:24 +0800

    feat: export interface, 方便本地调试类型

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
-import { Bot as BotType } from './Bot';
-import { Message as MessageType } from './Message';
-import { Middleware as MiddlewareType } from './Middleware';
+import { Bot, Bot as BotType } from './Bot';
+import { Message, Message as MessageType } from './Message';
+import { Middleware, Middleware as MiddlewareType } from './Middleware';
+
+export { Bot, Message, Middleware };
 declare module 'mirai-js' {
     export const Bot: typeof BotType;
     export const Message: typeof MessageType;
