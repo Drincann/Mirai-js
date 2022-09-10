db0f1ec474e0c222ef29c043511270385805b4d0
Drincann <1019933576@qq.com>
2022-09-10 19:32:01 +0800

    feat: Mirai WebSocket types

---
+++ src/types/WebSocketResponse.ts
@@ -0,0 +1,11 @@
+export interface MiraiWebSocketResponse {
+    syncId: string;
+    data: Record<string, any>;
+}
+
+export interface MiraiVerifiedWebSocketResponse extends MiraiWebSocketResponse {
+    data: {
+        session: string;
+        code: number;
+    }
+}
