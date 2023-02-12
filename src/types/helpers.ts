81d096b0ea25cb9539cc19f84d294592ea43cd98
gaojunkang <gaojunkang@xiaohongshu.com>
2023-02-13 03:07:54 +0800

    feat(type): 类型守护方法推断到的 intersection merge 起来

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,9 +1,10 @@
-import { EventMap } from ".";
+import { MiraiEventMap } from ".";
+import { Merge } from "./typeHelpers";
 
-export function isFriendMessageEventOrGroupMessage<T>(ctx: T): ctx is T & (EventMap['FriendMessage'] | EventMap['GroupMessage']) {
+export function isFriendMessageEventOrGroupMessage<T>(ctx: T): ctx is Merge<T & (MiraiEventMap['FriendMessage'] | MiraiEventMap['GroupMessage'])> {
     return (ctx as any).type === 'FriendMessage' || (ctx as any).type === 'GroupMessage'
 }
 
-export function textPropContext<T>(ctx: T): ctx is T & { text: string } {
+export function textPropContext<T>(ctx: T): ctx is Merge<T & { text: string }> {
     return true;
 }
